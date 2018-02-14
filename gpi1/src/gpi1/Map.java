b8bd6302a7f0ac0a8163d454a3a36dd5bb0c9004
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-02-14 08:55:12 +0000

    Map constructor

--- gpi1/src/gpi1/Map.java
+++ gpi1/src/gpi1/Map.java
@@ -1,7 +1,14 @@
 package gpi1;
 
 public class Map {
-	Map(String str) {
+	private char[][] map;
+	
+	public Map(String str) {
+		// Assuming the map it's a square
+		int n = (int) Math.sqrt(str.length());
 		
+		for(int i = 0; i < n; i++) {
+			map[i] = str.substring(0, n-1).toCharArray();
+		}
 	}
 }
