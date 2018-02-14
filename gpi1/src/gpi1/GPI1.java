0e5c5767fe42de6cb60c79ce20d3b4182239c2a4
Daniel Silva <danny@dannyps.net>
2018-02-14 09:01:03 +0000

    Auto stash before merge of "master" and "origin/master"

--- gpi1/src/gpi1/GPI1.java
+++ gpi1/src/gpi1/GPI1.java
@@ -5,6 +5,7 @@ public class GPI1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Start!");
+		@SuppressWarnings("unused")
+		Map map = new Map("XXXXXXXXXXXH  I X GXXXX XXX  XX I I X  XXXX XXX  XI        XI        XXXX XXXX XX I I XK XXXXXXXXXXX"); // our board
 	}
-
 }
