64e8cbeaa58e74eb131050810ba1dacc27e98fd4
Daniel Silva <danny@dannyps.net>
2018-02-21 09:34:51 +0000

    remove Level2 str arg

--- gpi1/src/Levels/Level2.java
+++ gpi1/src/Levels/Level2.java
@@ -3,7 +3,7 @@ import MapEntities.*;
 
 public class Level2 extends Map {
 
-	public Level2(String str) throws Exception {
+	public Level2() throws Exception {
 		super("XXXXXXXXXI   O  KXX       XX       XX       XX       XX       XXH      XXXXXXXXXX");
 		
 		exitDoors.add((Door)map[1][0]);
