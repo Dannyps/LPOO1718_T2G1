a33325d54f10ef732e30fe0cd4c1c295760f7a00
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-02-21 08:13:06 +0000

    Added level 2 string

--- gpi1/src/gpi1/Gameplay.java
+++ gpi1/src/gpi1/Gameplay.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 
 public class Gameplay {
 	// TODO add the second level to array
-	private String[] lvls = {"XXXXXXXXXXXH  I X GXXXX XXX  XX I I X  XXXX XXX  XI        XI        XXXX XXXX XX I I XK XXXXXXXXXXX"};
+	private String[] lvls = {"XXXXXXXXXXXH  I X GXXXX XXX  XX I I X  XXXX XXX  XI        XI        XXXX XXXX XX I I XK XXXXXXXXXXX",
+			"XXXXXXXXXXI   O  KXX       XX       XX       XX       XX       XXH      XXXXXXXXXX"
+			};
 	private Map map;
 	private int currentlvl;
 	public boolean gameEnd=false;
