4c72abfed5bad7f88d5570cb4da62e2caec293d8
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-02-21 10:00:33 +0000

    Added new class for Key entity

---
+++ gpi1/src/MapEntities/Key.java
@@ -0,0 +1,12 @@
+package MapEntities;
+
+import Levels.Map;
+
+public class Key extends GenericMapEntity {
+
+	public Key(int x, int y, Map map) {
+		super(x, y, map);
+		// TODO Auto-generated constructor stub
+	}
+
+}
