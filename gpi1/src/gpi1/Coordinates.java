d589976af93a333b119cbc81ea6ad696c35c964d
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-02-21 10:28:31 +0000

    Override the equals() method

--- gpi1/src/gpi1/Coordinates.java
+++ gpi1/src/gpi1/Coordinates.java
@@ -17,28 +17,12 @@ public class Coordinates {
 		this.y = c[1];
 	}
 	
-	// Nobody used this anyway
-	/*
-	
-	// getters
-	public int getX() {
-		return x;
-	}
-	
-	public int getY() {
-		return y;
-	}
-	
-	// setters
-	public void setX(int x) {
-		this.x = x;
-	}
-	
-	public void setY(int y) {
-		this.y = y;
-	}	*/
 	
 	public Coordinates clone() {
 		return new Coordinates(x,y);
+	}
+	
+	public boolean equals(Coordinates c) {
+		return (this.x == c.x && this.y == c.y);
 	}
 }
