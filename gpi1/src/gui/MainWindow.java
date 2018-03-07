422e6bd4035c1ac810b1f6064907fa3d49e67d66
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-03-07 10:27:29 +0000

    Added top panel

--- gpi1/src/gui/MainWindow.java
+++ gpi1/src/gui/MainWindow.java
@@ -3,10 +3,17 @@ package gui;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
 
 public class MainWindow {
 
 	private JFrame frame;
+	private JTextField ogreNo;
 
 	/**
 	 * Launch the application.
@@ -38,6 +45,21 @@ public class MainWindow {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JPanel panel = new JPanel();
+		FlowLayout flowLayout = (FlowLayout) panel.getLayout();
+		flowLayout.setHgap(10);
+		flowLayout.setAlignment(FlowLayout.LEFT);
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
+		
+		JLabel label1 = new JLabel("Number of ogres");
+		panel.add(label1);
+		
+		ogreNo = new JTextField();
+		ogreNo.setHorizontalAlignment(SwingConstants.RIGHT);
+		ogreNo.setText("2");
+		panel.add(ogreNo);
+		ogreNo.setColumns(3);
 	}
 
 }
