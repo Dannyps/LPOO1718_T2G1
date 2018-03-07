cf77707b0351379b53caeee4f4387ea93f294a4a
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-03-07 10:57:38 +0000

    Adjusting layout

--- gpi1/src/gui/MainWindow.java
+++ gpi1/src/gui/MainWindow.java
@@ -13,6 +13,9 @@ import javax.swing.JComboBox;
 import javax.swing.BoxLayout;
 import java.awt.GridLayout;
 import java.awt.Component;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
 
 public class MainWindow {
 
@@ -50,33 +53,56 @@ public class MainWindow {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.Y_AXIS));
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{432, 0};
+		gridBagLayout.rowHeights = new int[]{20, 20, 0};
+		gridBagLayout.columnWeights = new double[]{0.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
 		
-		// TOP PANEL for number of ogres input
+		/**
+		 * TOP PANEL for number of ogres input
+		 */
 		JPanel panel = new JPanel();
 		panel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		panel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
+		
+		// Constraints to align panel with main frame
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.anchor = GridBagConstraints.FIRST_LINE_START; // align top-left
+		gbc_panel.insets = new Insets(5, 5, 5, 5); // padding
+		gbc_panel.gridx = 0; // specify row
+		gbc_panel.gridy = 0; // specify column
+		gbc_panel.fill = GridBagConstraints.HORIZONTAL;
+		frame.getContentPane().add(panel, gbc_panel);
+		
 		// Label
 		JLabel label1 = new JLabel("Number of ogres");
 		panel.add(label1);
+		
 		// Textfield
 		ogreNo = new JTextField();
 		ogreNo.setHorizontalAlignment(SwingConstants.RIGHT);
 		ogreNo.setText("2");
-		panel.add(ogreNo);
 		ogreNo.setColumns(3);
+		panel.add(ogreNo);
 		
-		frame.getContentPane().add(panel);
 		
-		
-		// BOTTOM PANEL for guard personality
+		/**
+		 * Second panel for guard personality
+		 */
 		JPanel panel_1 = new JPanel();
-		frame.getContentPane().add(panel_1);
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 1;
+		gbc_panel_1.fill = GridBagConstraints.HORIZONTAL;
+		frame.getContentPane().add(panel_1, gbc_panel_1);
 		panel_1.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
 		
+		// label
 		JLabel lblNewLabel = new JLabel("New label");
 		panel_1.add(lblNewLabel);
-		
+		// combo
 		JComboBox comboBox = new JComboBox();
 		panel_1.add(comboBox);
 	}
