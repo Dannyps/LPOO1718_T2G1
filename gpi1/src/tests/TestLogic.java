d5bd0b65fcb6438276eaf946f1eee10275ad67d3
Daniel Silva <danny@dannyps.net>
2018-03-07 10:43:35 +0000

    Add simple test

---
+++ gpi1/src/tests/TestLogic.java
@@ -0,0 +1,17 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import cli.Gameplay;
+import logic.Levels.*;
+
+class TestLogic {
+
+	@Test
+	public void testMapGen() {
+
+	}
+
+}
