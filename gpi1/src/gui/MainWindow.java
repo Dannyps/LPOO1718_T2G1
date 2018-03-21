9721635a2acc5bd12114737b78b31eddf8e9efb1
Daniel Silva <danny@dannyps.net>
2018-03-21 09:01:22 +0000

    Add exit button

--- gpi1/src/gui/MainWindow.java
+++ gpi1/src/gui/MainWindow.java
@@ -25,7 +25,6 @@ import javax.swing.JButton;
 import javax.swing.JSplitPane;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.nio.file.DirectoryIteratorException;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 
@@ -66,29 +65,27 @@ public class MainWindow {
 	private void initialize() {
 		// MAIN FRAME
 		frame = new JFrame();
-		frame.setBounds(100, 100, 234, 461);
+		frame.setBounds(100, 100, 225, 495);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{432, 0};
-		gridBagLayout.rowHeights = new int[]{20, 20, 0, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
-		frame.getContentPane().setLayout(gridBagLayout);
 		
 		/**
 		 * TOP PANEL for number of ogres input
 		 */
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		gridBagLayout.columnWidths = new int[]{393, 0};
+		gridBagLayout.rowHeights = new int[]{38, 86, 27, 0};
+		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		frame.getContentPane().setLayout(gridBagLayout);
 		JPanel panel = new JPanel();
 		panel.setAlignmentX(Component.LEFT_ALIGNMENT);
 		panel.setLayout(new FlowLayout(FlowLayout.LEFT, 5, 5));
-		
-		// Constraints to align panel with main frame
 		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.anchor = GridBagConstraints.FIRST_LINE_START; // align top-left
-		gbc_panel.insets = new Insets(5, 5, 5, 0); // padding
-		gbc_panel.gridx = 0; // specify row
-		gbc_panel.gridy = 0; // specify column
+		gbc_panel.anchor = GridBagConstraints.NORTH;
 		gbc_panel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel.insets = new Insets(0, 0, 5, 0);
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
 		frame.getContentPane().add(panel, gbc_panel);
 		
 		// Label
@@ -102,6 +99,15 @@ public class MainWindow {
 		ogreNo.setColumns(3);
 		panel.add(ogreNo);
 		
+		try {
+			//UIManager.setLookAndFeel("NIMBUS");
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e2) {
+			// TODO Auto-generated catch block
+			e2.printStackTrace();
+		}
+		
 		JSplitPane splitPane = new JSplitPane();
 		splitPane.setEnabled(false);
 		GridBagConstraints gbc_splitPane = new GridBagConstraints();
@@ -111,15 +117,6 @@ public class MainWindow {
 		gbc_splitPane.gridx = 0;
 		gbc_splitPane.gridy = 1;
 		frame.getContentPane().add(splitPane, gbc_splitPane);
-		
-		try {
-			//UIManager.setLookAndFeel("NIMBUS");
-			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
-				| UnsupportedLookAndFeelException e2) {
-			// TODO Auto-generated catch block
-			e2.printStackTrace();
-		}
 		
 		JPanel moveButtonsPanel = new JPanel();
 		splitPane.setLeftComponent(moveButtonsPanel);
@@ -184,16 +181,6 @@ public class MainWindow {
 		JButton btnStartGame = new JButton("Start Game");
 		
 		splitPane.setRightComponent(btnStartGame);
-		
-		ta = new JTextArea();
-		ta.setFont(new Font("Courier New", Font.PLAIN, 13));
-		ta.setEditable(false);
-		ta.setLineWrap(true);
-		GridBagConstraints gbc_textArea = new GridBagConstraints();
-		gbc_textArea.fill = GridBagConstraints.BOTH;
-		gbc_textArea.gridx = 0;
-		gbc_textArea.gridy = 2;
-		frame.getContentPane().add(ta, gbc_textArea);
 		//frame.getContentPane().setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{label1, panel, ogreNo}));
 		
 		btnStartGame.addActionListener(new ActionListener() {
@@ -210,6 +197,41 @@ public class MainWindow {
 				((JButton) e.getSource()).setEnabled(false); // disable start game button
 			}
 		});
+		
+		JPanel panel_1 = new JPanel();
+		GridBagConstraints gbc_panel_1 = new GridBagConstraints();
+		gbc_panel_1.fill = GridBagConstraints.BOTH;
+		gbc_panel_1.gridx = 0;
+		gbc_panel_1.gridy = 2;
+		frame.getContentPane().add(panel_1, gbc_panel_1);
+		GridBagLayout gbl_panel_1 = new GridBagLayout();
+		gbl_panel_1.columnWidths = new int[]{393, 0};
+		gbl_panel_1.rowHeights = new int[]{27, 0, 0};
+		gbl_panel_1.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{1.0, 0.0, Double.MIN_VALUE};
+		panel_1.setLayout(gbl_panel_1);
+		
+		ta = new JTextArea();
+		GridBagConstraints gbc_ta = new GridBagConstraints();
+		gbc_ta.insets = new Insets(0, 0, 5, 0);
+		gbc_ta.fill = GridBagConstraints.BOTH;
+		gbc_ta.gridx = 0;
+		gbc_ta.gridy = 0;
+		panel_1.add(ta, gbc_ta);
+		ta.setFont(new Font("Courier New", Font.PLAIN, 13));
+		ta.setEditable(false);
+		ta.setLineWrap(true);
+		
+		JButton btnNewButton = new JButton("Exit");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 1;
+		panel_1.add(btnNewButton, gbc_btnNewButton);
 		
 	}
 	public void refreshTextArea() {
