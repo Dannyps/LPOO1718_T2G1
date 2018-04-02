88c2d901b70b7a58eabd84fc24a4234990fa56fa
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-04-02 23:16:47 +0100

    Added useful methods to Coordinates class

--- gpi1/src/logic/Coordinates.java
+++ gpi1/src/logic/Coordinates.java
@@ -1,6 +1,54 @@
 package logic;
 
 public class Coordinates {
+	
+	private int x, y;
+	/**
+	 * A pair holding coordinates
+	 * @param x (0-based)
+	 * @param y (0-based)
+	 */
+	public Coordinates(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	public Coordinates(int[] c) {
+		this.x = c[0];
+		this.y = c[1];
+	}
+	
+	// Some useful methods to update coordinates
+	public void moveLeft() {
+		this.x--;
+	}
+	
+	public void moveRight() {
+		this.x++;
+	}
+	
+	public void moveUp() {
+		this.y--;
+	}
+	
+	public void moveDown() {
+		this.y++;
+	}
+	
+	@Override
+	public String toString() {
+		return "{"+x+","+y+"}";
+	}
+	
+	@Override
+	public Coordinates clone() {
+		return new Coordinates(x,y);
+	}
+	
+	public boolean equals(Coordinates c) {
+		return (this.x == c.x && this.y == c.y);
+	}
+	
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -26,32 +74,4 @@ public class Coordinates {
 		return true;
 	}
 
-	public int x, y;
-	/**
-	 * a pair holding coordinates
-	 * @param x (0-based)
-	 * @param y (0-based)
-	 */
-	public Coordinates(int x, int y) {
-		this.x = x;
-		this.y = y;
-	}
-	
-	public Coordinates(int[] c) {
-		this.x = c[0];
-		this.y = c[1];
-	}
-	
-	
-	public Coordinates clone() {
-		return new Coordinates(x,y);
-	}
-	
-	public boolean equals(Coordinates c) {
-		return (this.x == c.x && this.y == c.y);
-	}
-	
-	public String toString() {
-		return "{"+x+","+y+"}";
-	}
 }
