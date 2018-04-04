dc9f586162c692fa268ad9f8fda05f648b0383be
Fábio Gaspar <fabiodanielreis@hotmail.com>
2018-04-04 10:13:32 +0100

    Refactored DrukenGuard

--- gpi1/src/logic/MapEntities/Guard/DrunkenGuard.java
+++ gpi1/src/logic/MapEntities/Guard/DrunkenGuard.java
@@ -2,18 +2,57 @@ package logic.MapEntities.Guard;
 
 import java.util.Random;
 
-import logic.Direction;
-import logic.Levels.Map;
-import logic.MapEntities.Hero;
+import logic.Coordinates;
 
 public class DrunkenGuard extends Guard {
 	private boolean sleeping;
 
-	public DrunkenGuard(int x, int y, Map map) {
-		super(x, y, map);
+	public DrunkenGuard(int x, int y, String route) {
+		super(x, y, route);
 		this.sleeping = false;
 	}
 
+	public Coordinates nextCoordinates() {
+		// previous sleeping state
+		boolean wasSleeping = this.sleeping;
+
+		// decide if the guard is going to / keeps sleeping on this iteration
+		if (randomGuardSleep()) {
+			// guard is sleeping, don't move
+			return this.coordinates;
+		} else if (wasSleeping) {
+			// if the guard was sleeping, and now is awake, maybe revert the route?
+			randomRevertRoute();
+		}
+		
+		// move the guard
+		return this.moveGuard();
+	}
+	
+	/**
+	 * Randomly wakes up the guard or set it to sleep
+	 * It updates the sleeping flag accordingly
+	 * 
+	 * @return true if the guard is now sleeping, otherwise returns false
+	 */
+	protected boolean randomGuardSleep() {
+		return this.sleeping = new Random().nextBoolean();
+	}
+	
+	/**
+	 * Randomly reverses the guard route
+	 */
+	protected void randomRevertRoute() {
+		if (new Random().nextBoolean()) {
+			// reverse route
+			for (int i = 0; i < route.length / 2; i++) {
+				char temp = route[i];
+				route[i] = route[route.length - i - 1];
+				route[route.length - i - 1] = temp;
+			}
+		}
+	}
+	
 	@Override
 	public String toString() {
 		if (sleeping) {
@@ -21,32 +60,25 @@ public class DrunkenGuard extends Guard {
 		} else
 			return "G";
 	}
-
+	
+	/*
 	@Override
 	public boolean tick() {
-		/**
-		 * *** If now is sleeping ...
-		 * ****** Do nothing
-		 * *** If now is awake ...
-		 * ***** and was sleeping
-		 * ******** maybe revert the route
-		 * ***** move the guard
-		 */
 
 		// previous sleeping state
 		boolean wasSleeping = this.sleeping;
-		
+
 		// decide if the guard is sleeping on this iteration
-		if(randomGuardSleep()) {
+		if (randomGuardSleep()) {
 			// guard is sleeping, do nothing
 			return true;
 		} else {
 			// if the guard was sleeping, and now is awake, maybe revert the route?
-			if(wasSleeping) {
+			if (wasSleeping) {
 				randomRevertRoute();
 			}
 		}
-		
+
 		// move the guard
 		this.moveGuard(route[index]);
 		if (index == route.length - 1)
@@ -64,24 +96,6 @@ public class DrunkenGuard extends Guard {
 
 		return true;
 	}
-
-	/**
-	 * Randomly wakes up the guard or set it to sleep
-	 * It changes the flag sleeping
-	 * @return true if the guard is now sleeping
 	 */
-	protected boolean randomGuardSleep() {
-		return this.sleeping = new Random().nextBoolean();
-	}
-
-	protected void randomRevertRoute() {
-		if (new Random().nextBoolean()) {
-			// reverse route
-			for (int i = 0; i < route.length / 2; i++) {
-				char temp = route[i];
-				route[i] = route[route.length - i - 1];
-				route[route.length - i - 1] = temp;
-			}
-		}
-	}
+	
 }
