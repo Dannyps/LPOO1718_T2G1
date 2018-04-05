b2926e464f40d593d373c8431a88d8f2184ee826
Daniel Silva <danny@dannyps.net>
2018-04-05 15:26:17 +0100

    Parse Ogres, Hero and Walls images

--- gpi1/src/gui/GameViewPanel.java
+++ gpi1/src/gui/GameViewPanel.java
@@ -2,6 +2,9 @@ package gui;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.awt.image.BufferedImage;
+
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 import cli.Gameplay;
@@ -14,6 +17,10 @@ public class GameViewPanel extends JPanel implements MouseListener, MouseMotionL
 
 	Gameplay game = null;
 
+	int qs;
+
+	Image IHero, IWall, IGuard, IOgre, IClub, IKey, ILever;
+
 	public void updateGame(Gameplay game) {
 		this.game = game;
 	}
@@ -23,49 +30,124 @@ public class GameViewPanel extends JPanel implements MouseListener, MouseMotionL
 		addMouseListener(this);
 		addMouseMotionListener(this);
 		addKeyListener(this);
+		loadImages();
+	}
+
+	private void loadImages() {
+		IHero = new ImageIcon("assets/hero.png").getImage();
+		IWall = new ImageIcon("assets/wall.png").getImage();
+		IOgre = new ImageIcon("assets/ogre.png").getImage();
+
 	}
 
 	// Redraws the panel, only when requested by SWING
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g); // limpa fundo …
 		if (game != null)
-			drawGrid(g, 10, 10, 30);
+			drawGrid(g, 10, 10);
 		g.setColor(Color.BLUE);
 		g.fillOval(x1, y1, x2 - x1 + 1, y2 - y1 + 1);
-		// System.out.println(getSize());
 	}
 
 	/**
 	 * Draws the gameboard on the specified coordinates
 	 * 
-	 * @param g  Graphics
-	 * @param x  int
-	 * @param y  int
-	 * @param qs int quadricule size
+	 * @param g Graphics
+	 * @param x int
+	 * @param y int
 	 *
 	 */
-	private void drawGrid(Graphics g, int x, int y, int qs) {
+	private void drawGrid(Graphics g, int x, int y) {
 		int ss = game.getLevel().getMap().length; // squareSide
-		qs = (int) (Integer.min(getSize().width, getSize().height)/(ss+1));
+		qs = (int) (Integer.min(getSize().width, getSize().height) / (ss + 1));
 
-		makeWalls(g, x, y, qs, ss);
-
-		/*
-		 * for(GenericMapEntity[] mapRows : game.getLevel().getMap()) {
-		 * for(GenericMapEntity e : mapRows) {
-		 * 
-		 * } }
-		 */
+		makeGrid(g, x, y, qs, ss);
+		for (GenericMapEntity[] mapRows : game.getLevel().getMap()) {
+			for (GenericMapEntity e : mapRows) {
+				int ex = e.getCoordinates().x;
+				int ey = e.getCoordinates().y;
+				
+				drawOnQuadricule(g, ey, ex, e.getClass().getName());
+			}
+		}
 	}
 
 	/**
-	 * @param g Graphics 
-	 * @param x int
-	 * @param y int
+	 * Draws Hero to g
+	 * @param g Graphics
+	 * @param x int x coordinate
+	 * @param y int y coordinate
+	 */
+	private void drawHero(Graphics g, int x, int y) {
+		double ratio = IHero.getWidth(null) / IHero.getHeight(null);
+		double lqs = qs * 0.95;
+
+		g.drawImage(IHero, x + 12, y + 10, (int) (lqs), (int) (lqs * ratio), null);
+	}
+
+	/**
+	 * Draws Wall to g
+	 * @param g Graphics
+	 * @param x int x coordinate
+	 * @param y int y coordinate
+	 */
+	private void drawWall(Graphics g, int x, int y) {
+		double ratio = IWall.getWidth(null) / IWall.getHeight(null);
+		double lqs = qs * 0.95;
+
+		g.drawImage(IWall, x + 14, y + 12, (int) (lqs), (int) (lqs * ratio), null);
+	}
+	
+	/**
+	 * Draws Wall to g
+	 * @param g Graphics
+	 * @param x int x coordinate
+	 * @param y int y coordinate
+	 */
+	private void drawOgre(Graphics g, int x, int y) {
+		double ratio = IWall.getWidth(null) / IWall.getHeight(null);
+		double lqs = qs * 0.95;
+
+		g.drawImage(IOgre, x + 14, y + 12, (int) (lqs), (int) (lqs * ratio), null);
+	}
+
+	/**
+	 * Draws the specified entity onto the grid
+	 * @param g Graphics
+	 * @param x int grid x
+	 * @param y int grid y
+	 * @param ent String representing the Entity's class name.
+	 */
+	void drawOnQuadricule(Graphics g, int x, int y, String ent) {
+		int coords[] = calcCoordByQuadricule(x, y);
+		switch (ent.substring(ent.lastIndexOf(".") + 1)) {
+		case "Hero":
+			drawHero(g, coords[0], coords[1]);
+			break;
+		case "Wall":
+			drawWall(g, coords[0], coords[1]);
+			break;
+		case "Ogre":
+			drawOgre(g, coords[0], coords[1]);
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	private int[] calcCoordByQuadricule(int x, int y) {
+		return new int[] { x * qs, y * qs };
+	}
+
+	/**
+	 * @param g  Graphics
+	 * @param x  int
+	 * @param y  int
 	 * @param qs int
 	 * @param ss int
 	 */
-	private void makeWalls(Graphics g, int x, int y, int qs, int ss) {
+	private void makeGrid(Graphics g, int x, int y, int qs, int ss) {
 		for (int i = 0; i <= ss; i++) {
 			g.drawLine(x + i * qs, y, x + i * qs, y + ss * qs);
 		}
