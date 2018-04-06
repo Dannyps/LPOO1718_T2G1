99e296d4124cb840a0c3f19f9e16f571e76b8e1b
Daniel Silva <danny@dannyps.net>
2018-04-06 17:03:23 +0100

    Tests fail without this one

--- gpi1/src/logic/Coordinates.java
+++ gpi1/src/logic/Coordinates.java
@@ -3,8 +3,10 @@ package logic;
 public class Coordinates {
 
 	public int x, y;
+
 	/**
 	 * a pair holding coordinates
+	 * 
 	 * @param x (0-based)
 	 * @param y (0-based)
 	 */
@@ -12,12 +14,28 @@ public class Coordinates {
 		this.x = x;
 		this.y = y;
 	}
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Coordinates other = (Coordinates) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
+
 	public boolean equals(Coordinates c) {
 		return (this.x == c.x && this.y == c.y);
 	}
-	
+
 	public String toString() {
-		return "{"+x+","+y+"}";
+		return "{" + x + "," + y + "}";
 	}
 }
